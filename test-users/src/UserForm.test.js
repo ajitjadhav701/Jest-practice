29a2e8fed685e351e48f5221c6fed2f1854b5927
ajitjadhav701 <ajitjadhav701@gmail.com>
2023-08-25 12:13:42 +0530

    try to simulate user events and recording function calls

--- test-users/src/UserForm.test.js
+++ test-users/src/UserForm.test.js
@@ -14,3 +14,34 @@ test('user form shows two inputs and a button',()=>{
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
 });
+
+test('when form is submitted it calls onUserAdd',()=>{
+    //not a best implementation
+    const argList=[];
+    const callBack=(...args) =>{
+        argList.push(args);
+    }
+    //try to render component
+    render(<UserForm onUserAdd={callBack}/>);
+
+    //find two inputsw
+    const [nameInput,emailinput] = screen.getAllByRole('textbox');
+
+    //Simulate typing in a name field
+    user.click(nameInput);
+    user.keyboard('Jane');
+
+    //Simulate typing in a email field
+    user.click(emailinput);
+    user.keyboard('jane123@gmail.com');
+
+    //find the button
+    const button=screen.getByRole('button');
+
+    //Simulate clicking a button
+    user.click(button);
+
+    //Assertion to make sure 'onUserAdd' is called with name/email
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({name:'Jane',email:'jane123@gmail.com'});
+})
