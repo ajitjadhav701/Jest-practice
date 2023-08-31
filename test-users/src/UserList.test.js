49c14e238e31d512ee7e5d9c52005e74e1c2b391
ajitjadhav701 <ajitjadhav701@gmail.com>
2023-08-31 11:04:45 +0530

    second test of userList component is done

--- test-users/src/UserList.test.js
+++ test-users/src/UserList.test.js
@@ -1,13 +1,26 @@
 import {render,screen,within} from '@testing-library/react';
 import UserList from './UserList';
 
+
+
+function renderComponent(){
+    const users = [
+        { name: 'jane', email: 'jane@gmail.com' },
+        { name: 'sam', email: 'sam@gmail.com' },
+    ]
+    render(<UserList users={users} />);
+
+    return {
+        users,
+    };
+}
+// //beforeEach fun
+// beforeEach(()=>{
+// render(<UserList users={users} />);//not allowed to render compo incide it
+// })/
 test('render one row per user', () => {
     //render the components
-    const users=[
-        {name: 'jane',email:'jane@gmail.com'},
-        {name: 'sam',email:'sam@gmail.com'},
-    ]
-    const {container}=render(<UserList users={users}/>);
+    renderComponent();
 
     //find all the rows in table
     // screen.logTestingPlaygroundURL();//to select the element by going in to playground link
@@ -18,14 +31,25 @@ test('render one row per user', () => {
 
     //2.container.querySelector();
     //eslint-disable-next-line  //just ignore next line warning
-    const rows = container.querySelectorAll('tbody tr');
+    // const rows = container.querySelectorAll('tbody tr');
+    
+    const rows = within(screen.getByTestId('users')).getAllByRole('row');
     
     //Assertion correct number of rows in a table
     expect(rows).toHaveLength(2);
 
-    
 });
 
 test('render the email and name of each user', () => {
 
+   const {users}=renderComponent();
+    // screen. logTestingPlaygroundURL(); 
+
+    for (let user of users){
+        const name=screen.getByRole('cell',{name:user.name});
+        const email=screen.getByRole('cell',{name:user.email});
+ 
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    }
 });
